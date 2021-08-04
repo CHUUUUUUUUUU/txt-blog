d3bcc29d04bb13355da6e40aab84f6764b18defc
Alone <27599588+endupalone@users.noreply.github.com>
2021-08-04 14:20:37 +0800

    update

--- txt.sh
+++ txt.sh
@@ -7,8 +7,8 @@ fi
 
 cp -r src/* build/
 
-HEAD='<!DOCTYPE html><html><head><meta charset="utf-8" /><meta name=viewport content="width=device-width,initial-scale=1"><style>body{font-family:sans-serif;font-size:20px;margin:20px;max-width:600px;}a{color:#345cde;text-decoration:none;}pre{white-space:pre-wrap;word-wrap:break-word;}@media(prefers-color-scheme:dark){body{background:#000;color:#fff}a{color:#fdc9f7}}</style></head><body>'
-FOOTER="</body></html>"
+top='<!DOCTYPE html><html><head><meta charset="utf-8" /><meta name=viewport content="width=device-width,initial-scale=1"><link href="#" rel=icon><style>body{font-family:sans-serif;font-size:20px;margin:20px;max-width:600px;}a{color:#345cde;text-decoration:none;}pre{white-space:pre-wrap;word-wrap:break-word;}@media(prefers-color-scheme:dark){body{background:#000;color:#fff}a{color:#fdc9f7}}</style></head><body>'
+bottom="</body></html>"
 
 txt="src/p/*"
 
@@ -19,13 +19,13 @@ do
 
     pre="$(cat src/p/$POST_FILE)"
     
-    echo "$HEAD<pre>$pre</pre>$FOOTER" > "build/p/${POST_NAME}.html"
+    echo "$top<pre>$pre</pre>$bottom" > "build/p/${POST_NAME}.html"
 
     txtlist="$txtlist<a href=\"/p/$POST_FILE\">$POST_FILE</a><br />"
     prelist="$prelist<a href=\"/p/$POST_NAME\">$POST_NAME</a><br />"
 
-    echo "$HEAD <p>Posts</p> $prelist $FOOTER" > "build/p.html"
-    echo "$HEAD <p>txt</p> $txtlist $FOOTER" > "build/txt.html"
+    echo "$top <p>Posts</p> $prelist $bottom" > "build/p.html"
+    echo "$top <p>txt</p> $txtlist $bottom" > "build/txt.html"
 done
 
 
@@ -35,7 +35,7 @@ do
     PAGE_NAME=$(basename $page .txt)
     
     content="$(cat src/$PAGE_FILE)"
-    echo "$HEAD<pre>$content</pre>$FOOTER" > "build/${PAGE_NAME}.html"
+    echo "$top<pre>$content</pre>$bottom" > "build/${PAGE_NAME}.html"
 done
 
 
